949f4a20bab0facca2bfac7af49daac02bec243c
ydahal1 <yadhapdahal@yadhaps-Mini.attlocal.net>
2020-12-31 15:41:56 -0500

    done

--- src/context.js
+++ src/context.js
@@ -14,6 +14,7 @@ const reducer = (state, action) => {
       return state;
   }
 };
+
 export class Provider extends Component {
   state = {
     track_list: [],
@@ -29,7 +30,6 @@ export class Provider extends Component {
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
-        // console.log(res.data);
         this.setState({
           track_list: res.data.message.body.track_list,
           heading: "Top 10 Tracks"
